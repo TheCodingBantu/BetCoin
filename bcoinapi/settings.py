b38440af04d772bb1382ca0ab8f5d49084f67264
Ngugi Phil <113933822+DevNgugi@users.noreply.github.com>
2022-11-17 13:43:03 +0300

    Update settings.py

--- bcoinapi/settings.py
+++ bcoinapi/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%%$#)02r$g&w6s6ew_%6_!knv-7t=e3%iwltz5j9x_1$-4)ie!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
